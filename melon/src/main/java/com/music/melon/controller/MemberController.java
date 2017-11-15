771b731a5b4d6cb70a43d95537f5de9a48fa15e8
노지형 <nrptu_000@NoJiSu>
2017-11-15 19:00:55 +0900

    회원가입

---
+++ melon/src/main/java/com/music/melon/controller/MemberController.java
@@ -0,0 +1,40 @@
+package com.music.melon.controller;
+
+import java.util.Locale;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.music.melon.HomeController;
+import com.music.melon.dao.MemberDAO;
+import com.music.melon.dto.Member_dto;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private MemberDAO memberDAO;
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	// 회원가입
+	@ResponseBody
+	@RequestMapping("/join")
+	String join(Member_dto member_dto, 
+			@RequestHeader(required = false, value = "referer", defaultValue = "/") String referer) throws Exception {
+		if (member_dto != null)
+			memberDAO.insert(member_dto);
+		return referer;
+	}
+	
+}
---
+++ melon/src/main/java/com/music/melon/dto/Member_dto.java
@@ -0,0 +1,87 @@
+package com.music.melon.dto;
+
+public class Member_dto {
+	 private int member_no;
+	 private String member_id;
+	 private String password;
+	 private String name;
+	 private String birth;
+	 private String email;
+	 private String phone;
+	 private String status;
+	 private String authority;
+	 private String enabled;
+	 private String reg;
+	 
+	public Member_dto() {
+		super();
+	}
+	
+	public int getMember_no() {
+		return member_no;
+	}
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	public String getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(String enabled) {
+		this.enabled = enabled;
+	}
+	public String getReg() {
+		return reg;
+	}
+	public void setReg(String reg) {
+		this.reg = reg;
+	}
+
+}
