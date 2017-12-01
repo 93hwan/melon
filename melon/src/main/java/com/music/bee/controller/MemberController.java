0d161b67021c5f29da1800c22c68f46be1125403
노지형 <nrptu_000@NoJiSu>
2017-12-01 14:43:22 +0900

    로그인 성공시 핸들러 설정

--- melon/src/main/java/com/music/bee/controller/MemberController.java
+++ melon/src/main/java/com/music/bee/controller/MemberController.java
@@ -2,13 +2,14 @@ package com.music.bee.controller;
 
 
 
+
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,9 +34,12 @@ public class MemberController {
 	// 회원가입
 	@ResponseBody
 	@RequestMapping("/join")
-	String join(Member_dto member_dto, 
+	String join(UsernamePasswordAuthenticationToken token,Member_dto member_dto,
 			@RequestHeader(required = false, value = "referer", defaultValue = "/") String referer) throws Exception {
 		logger.debug("member_dto = " + member_dto.toString());
+		if (token != null) {
+			throw new Exception("이미 로그인된 상태입니다.");
+		}
 		String securePassword = encoder.encode(member_dto.getPassword());
 		logger.debug("password = " + member_dto.getPassword());
 		member_dto.setPassword(securePassword);
@@ -44,11 +48,4 @@ public class MemberController {
 		memberDAO.join(member_dto);
 		return referer;
 	}
-	
-	//로그인
-	@RequestMapping("/login")
-	String login(Member_dto member_dto) {
-		return "";
-	}
-	
 }
